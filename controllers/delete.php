4bcc1189e9bd9db84ba73b92966feb40464d9d40
JustKhal <89084690+JustKhal@users.noreply.github.com>
2023-11-20 12:32:44 +0700

    Fix deletion process

---
+++ controllers/delete.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login']) || $_SESSION['role'] !== 'admin') {
+    header("Location: login.php");
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['message_id'])) {
+        $messageId = $_POST['message_id'];
+
+        require "connection.php";
+
+        // Retrieve the attachment path before deleting the message
+        $attachmentPathQuery = "SELECT attachment FROM communications WHERE id = ?";
+        $attachmentPathStmt = $db->prepare($attachmentPathQuery);
+        $attachmentPathStmt->bind_param("i", $messageId);
+        $attachmentPathStmt->execute();
+        $attachmentPathStmt->bind_result($attachmentPath);
+        $attachmentPathStmt->fetch();
+        $attachmentPath = "../" . $attachmentPath;
+        $attachmentPathStmt->close();
+
+        // Delete the message from the database
+        $deleteMessageQuery = "DELETE FROM communications WHERE id = ?";
+        $deleteMessageStmt = $db->prepare($deleteMessageQuery);
+        $deleteMessageStmt->bind_param("i", $messageId);
+
+        if ($deleteMessageStmt->execute()) {
+            // Delete the attachment file from the server
+            if (!empty($attachmentPath) && file_exists($attachmentPath)) {
+                unlink($attachmentPath);
+            }
+
+            $_SESSION['delete_success'] = "Message deleted successfully";
+        } else {
+            $_SESSION['delete_error'] = "Error deleting the message";
+        }
+
+        $deleteMessageStmt->close();
+        $db->close();
+    }
+}
+
+// Redirect back to the admin page
+header("Location: ../admin.php");
+exit();
+?>
