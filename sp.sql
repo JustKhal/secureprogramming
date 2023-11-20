6a11328040bf7013c51a93a4c42bb095df19dcfb
JustKhal <89084690+JustKhal@users.noreply.github.com>
2023-11-20 11:48:22 +0700

    Update SQL database

--- sp.sql
+++ sp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2023 at 04:29 PM
+-- Generation Time: Nov 20, 2023 at 05:47 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -18,8 +18,53 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tranqsite`
+-- Database: `sp`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `app_config`
+--
+
+CREATE TABLE `app_config` (
+  `key` varchar(15) NOT NULL,
+  `value` tinyint(1) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `app_config`
+--
+
+INSERT INTO `app_config` (`key`, `value`, `created_at`) VALUES
+('initialized', 1, '2019-04-30 07:13:37');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `communications`
+--
+
+CREATE TABLE `communications` (
+  `id` int(11) NOT NULL,
+  `sender_id` int(10) UNSIGNED NOT NULL,
+  `recipient_id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(64) NOT NULL,
+  `message` text NOT NULL,
+  `send_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `attachment` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `communications`
+--
+
+INSERT INTO `communications` (`id`, `sender_id`, `recipient_id`, `title`, `message`, `send_at`, `attachment`) VALUES
+(2, 1, 1, 'Important Request', 'Dear admin,\r\nwe need backup now!\r\nA virus is attacking and we are low on supply! SOS!', '2019-05-03 06:02:33', NULL),
+(3, 1, 4, 'Score Recap Request', 'Dude, please send me score recap from last night. Need it for the boss. Thanks!', '2019-05-03 06:13:10', NULL),
+(4, 2, 3, 'superbum', '123123', '2019-08-02 05:22:25', NULL),
+(5, 2, 2, 'qweqwe', 'dasdadasd', '2019-08-02 05:23:36', NULL);
 
 -- --------------------------------------------------------
 
@@ -43,14 +88,28 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fullname`, `username`, `email`, `role`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'test1', 'test1', 'test1@test.com', 'user', 'test1test1', '2019-04-30 07:13:37', '2023-11-19 15:28:52'),
-(2, 'test2', 'test2', 'test2@test.com', 'user', 'test2test2', '2019-04-30 07:13:37', '2023-11-19 15:28:52'),
-(3, 'test3', 'test3', 'test3@test.com', 'user', 'test3test3', '2019-04-30 07:13:37', '2023-11-19 15:28:52'),
-(5, 'admin', 'admin', 'admin@gmail.com', 'admin', 'admin', '2019-04-30 07:13:37', '2023-11-19 15:28:52');
+(1, 'test1', 'test1', 'test1@test.com', 'user', 'test1test1', '2019-04-30 07:13:37', '2023-11-20 03:54:17'),
+(2, 'test2', 'test2', 'test2@test.com', 'user', 'test2test2', '2019-04-30 07:13:37', '2023-11-20 03:54:17'),
+(3, 'test3', 'test3', 'test3@test.com', 'user', 'test3test3', '2019-04-30 07:13:37', '2023-11-20 03:54:17'),
+(5, 'admin', 'admin', 'admin@gmail.com', 'admin', 'admin', '2019-04-30 07:13:37', '2023-11-20 03:54:17');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `app_config`
+--
+ALTER TABLE `app_config`
+  ADD PRIMARY KEY (`key`);
+
+--
+-- Indexes for table `communications`
+--
+ALTER TABLE `communications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `foreign_constraint_recipient` (`recipient_id`),
+  ADD KEY `foreign_constraint_sender` (`sender_id`);
 
 --
 -- Indexes for table `users`
@@ -63,10 +122,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `communications`
+--
+ALTER TABLE `communications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `communications`
+--
+ALTER TABLE `communications`
+  ADD CONSTRAINT `foreign_constraint_sender` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
